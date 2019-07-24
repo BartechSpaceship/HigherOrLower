4538fb74b775ee219bcd5e7a2d70e517ed23ab1f
BartechSpaceship <bartek.daniel24@gmail.com>
2019-07-23 20:39:26 -0700

    Random
    This is where the random starts

--- app/src/main/java/com/example/higherorlower/MainActivity.java
+++ app/src/main/java/com/example/higherorlower/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+//this is where it starts
         Random rand = new Random();
 
         randomNumber = rand.nextInt(20) + 1;
